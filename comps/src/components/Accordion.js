be83de81c33672e2688c14ef23b65d618bc33f34
JiyuLei <jerrylei.ui@gmail.com>
2024-08-20 17:36:59 -0700

    Jiyu | Define handleClick function outside the mapping function

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -3,16 +3,17 @@ import { useState } from "react";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  // define handleClick outside the map function
+  const handleClick = (index) => {
+    setExpandedIndex(index);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
-      <div
-        key={item.id}
-        onClick={() => {
-          setExpandedIndex(index);
-        }}
-      >
+      // use handleClick function as inline
+      <div key={item.id} onClick={() => handleClick(index)}>
         <div>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
