d94a1f270b849c2a6661aa5dcc2070705101c3b2
JiyuLei <jerrylei.ui@gmail.com>
2024-08-20 17:09:55 -0700

    Jiyu | Create a state to manage conditional rendering

--- comps/src/App.js
+++ comps/src/App.js
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "aaa",
       label: "Gibson",
       content:
         "Built for comfort, Gibson's Studio acoustic guitars feature featherlight nitrocellulose satin finishes, player-friendly advanced response necks, and full body",
     },
     {
+      id: "bbb",
       label: "Fender",
       content:
         "There's nothing like playing a genuine Fender electric guitar—the sound that both created and redefined musical possibilities. ",
     },
     {
+      id: "ccc",
       label: "Prs",
       content:
         "Paul Reed Smith Guitars, also known as PRS Guitars or simply PRS, is an American guitar and amplifier manufacturer located in Stevensville, Maryland.",
